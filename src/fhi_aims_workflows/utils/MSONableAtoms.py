553f5385e9957c8b3f9d1b723c48a2403f4c031f
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2023-06-27 13:30:12 +0200

    MSONableAtoms is now a child of ase.atoms.Atoms

--- src/fhi_aims_workflows/utils/MSONableAtoms.py
+++ src/fhi_aims_workflows/utils/MSONableAtoms.py
@@ -2,14 +2,49 @@ from ase.atoms import Atoms
 from monty.json import MSONable, MontyDecoder
 
 
-class MSONableAtoms(MSONable):
-    def __init__(self, atoms):
-        self._atoms = atoms
+class MSONableAtoms(Atoms, MSONable):
+    def __init__(
+        self,
+        symbols=None,
+        positions=None,
+        numbers=None,
+        tags=None,
+        momenta=None,
+        masses=None,
+        magmoms=None,
+        charges=None,
+        scaled_positions=None,
+        cell=None,
+        pbc=None,
+        celldisp=None,
+        constraint=None,
+        calculator=None,
+        info=None,
+        velocities=None,
+    ):
+        super().__init__(
+            symbols,
+            positions,
+            numbers,
+            tags,
+            momenta,
+            masses,
+            magmoms,
+            charges,
+            scaled_positions,
+            cell,
+            pbc,
+            celldisp,
+            constraint,
+            calculator,
+            info,
+            velocities,
+        )
 
     def as_dict(self):
         d = {"@module": self.__class__.__module__, "@class": self.__class__.__name__}
 
-        for key, val in self.atoms.todict().items():
+        for key, val in self.todict().items():
             d[key] = val
 
         return d
@@ -22,9 +57,4 @@ class MSONableAtoms(MSONable):
             if not k.startswith("@")
         }
 
-        atoms = Atoms.fromdict(decoded)
-        return cls(atoms)
-
-    @property
-    def atoms(self):
-        return self._atoms
+        return super().fromdict(decoded)
