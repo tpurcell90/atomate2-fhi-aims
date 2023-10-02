d3c2bab4cb6fa0087f23c5fb7f162c243e4b7b44
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2023-10-02 20:56:47 +0200

    Run black on all test files

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,8 +8,8 @@ from pathlib import Path
 def compare_files(test_name, work_dir, ref_dir):
     for file in glob(f"{work_dir / test_name}/*in"):
         assert (
-                open(file, "rt").readlines()[4:]
-                == open(f"{ref_dir / test_name / Path(file).name}", "rt").readlines()[4:]
+            open(file, "rt").readlines()[4:]
+            == open(f"{ref_dir / test_name / Path(file).name}", "rt").readlines()[4:]
         )
     ref = json.load(open(f"{ref_dir / test_name}/parameters.json"))
     ref.pop("species_dir", None)
